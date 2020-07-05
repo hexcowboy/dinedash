63a7fada7353ea5f505d8d3bce788531058318d2
jackno <jacknovotny8@gmail.com>
2020-07-05 14:54:59 -0700

    static test;

--- config/settings/production.py
+++ config/settings/production.py
@@ -20,10 +20,10 @@ AWS_S3_CUSTOM_DOMAIN = os.environ['S3_CUSTOM_DOMAIN']
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'static'
+AWS_LOCATION = 'static-dinedash'
 AWS_DEFAULT_ACL = 'public-read'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-STATIC_ROOT = 'static-dinedash/'
+STATIC_ROOT = 'static/'
