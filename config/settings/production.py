b77e6eb4db76e294bb693921c9d6932a65ae454e
jackno <jacknovotny8@gmail.com>
2020-07-05 15:05:32 -0700

    change s3 endpoint

--- config/settings/production.py
+++ config/settings/production.py
@@ -16,14 +16,14 @@ AWS_SECRET_ACCESS_KEY = os.environ['S3_ACCESS_SECRET']
 AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 AWS_S3_ENDPOINT_URL = os.environ['S3_ENDPOINT']
 AWS_S3_CUSTOM_DOMAIN = os.environ['S3_CUSTOM_DOMAIN']
+AWS_LOCATION = 'static-dinedash'
 
+AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'static-dinedash'
-AWS_DEFAULT_ACL = 'public-read'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATIC_ROOT = 'static/'
