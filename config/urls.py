4dac40f06954c78b0d2c8e8543e77b208ca9c58c
jackno <jacknovotny8@gmail.com>
2020-07-07 11:54:33 -0700

    make empty home page

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path("admin/", admin.site.urls),
     path("menus/", include("dinedash.menu.urls")),
 ]
